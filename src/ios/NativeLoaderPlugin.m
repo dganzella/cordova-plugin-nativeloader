53835a26d36b15a5a0874390bf622443fa103b76
Danilo Ganzella <dganzella@2mundos.net>
2017-08-08 13:58:54 -0300

    Added loader anim on ios.

--- src/ios/NativeLoaderPlugin.m
+++ src/ios/NativeLoaderPlugin.m
@@ -1,43 +1,53 @@
 #import "NativeLoaderPlugin.h"
 
 @implementation NativeLoaderPlugin{
-	UIView* loaderView;
-	bool isViewShown;
+    UIView* bgView;
+    UIImageView * loaderView;
+    bool isViewShown;
 }
 
 #pragma mark -
 #pragma mark Cordova Methods
 -(void) pluginInitialize
 {
-	int viewTag = 7876;
-	
-	loaderView = [self.webView.superview viewWithTag: viewTag];
-	
-	if(loaderView == nil)
-	{
-		loaderView = [[UIView alloc] initWithFrame:CGRectMake(0,0,
-													 [[UIScreen mainScreen] applicationFrame].size.width,
-													 [[UIScreen mainScreen] applicationFrame].size.height)];
-													 
-		[loaderView setBackgroundColor:[UIColor blackColor]];
-		loaderView.tag = viewTag;	
-	}
+    
+    int animViewTag = 7876;
+    int bgViewTag = 7877;
+    
+    loaderView = (UIImageView*)[self.webView.superview viewWithTag: animViewTag];
+    bgView = [self.webView.superview viewWithTag: bgViewTag];
+    
+    if(loaderView == nil)
+    {
+        loaderView = [[UIImageView alloc] initWithFrame:CGRectMake(0,0,64,64)];
+        loaderView.image= [UIImage animatedImageNamed:@"preloader" duration:1.2f];
+        loaderView.contentMode = UIViewContentModeCenter;
+        
+        bgView = [[UIView alloc] initWithFrame:CGRectMake(0,0,
+                                                          [[UIScreen mainScreen] bounds].size.width,
+                                                          [[UIScreen mainScreen] bounds].size.height)];
+        
+        [bgView setBackgroundColor:[UIColor whiteColor]];
+        bgView.tag = bgViewTag;
+    }
 }
 
 -(void)showView:(CDVInvokedUrlCommand*)command
 {
-	bool showView = [[command.arguments objectAtIndex:0] boolValue];
-	NSLog(@"show view %@", isPublisher ? @"SHOW" : @"HIDE" );
-	
-	if(showView)
-	{
-		[self.webView.superview insertSubview:self.loaderView atIndex:999];
-	}
-	else
-	{
-		[self.loaderView removeFromSuperview];
-	}	
-	
+    bool showView = [[command.arguments objectAtIndex:0] boolValue];
+    NSLog(@"show view %@", showView ? @"SHOW" : @"HIDE" );
+    
+    if(showView)
+    {
+        [self.webView.superview insertSubview:bgView atIndex:50];
+        [self.webView.superview insertSubview:loaderView atIndex:51];
+    }
+    else
+    {
+        [loaderView removeFromSuperview];
+        [bgView removeFromSuperview];
+    }
+    
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
